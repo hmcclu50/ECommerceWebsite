2ba4747b9031b84a60eb5875bbeb9fa1a6525826
Alyssa Scudder <amscud69@thomasmore.edu>
2021-11-16 11:15:48 -0500

    Update DB

--- Data/DbInitializer.cs
+++ Data/DbInitializer.cs
@@ -20,6 +20,9 @@ namespace ECommerceWebsite.Data
                 new Product{ProductName="Dell PC",ProductPrice=700, ProductDescription="OptiPlex 3080 Desktop PC - Dell"},
                 new Product{ProductName="HP Laptop",ProductPrice=600, ProductDescription="17.3 Inch Laptop - HP"},
                 new Product{ProductName="ViewSonic Monitor",ProductPrice=150, ProductDescription="24 Inch Monitor - ViewSonic"},
+                new Product{ProductName="Microsft Mouse",ProductPrice=45, ProductDescription="Bluetooth Mouse - Microsoft"},
+                new Product{ProductName="Microsft Keyboard",ProductPrice=50, ProductDescription="Bluetooth Keyboard - Microsoft"},
+                new Product{ProductName="Logitech Headset",ProductPrice=75, ProductDescription="Bluetooth Headset - Logitech"},
             };
 
             context.Product.AddRange(products);
